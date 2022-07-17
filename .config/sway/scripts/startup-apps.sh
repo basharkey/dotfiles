f6ec302b84b5e9404976e61d816701be273331ae
basharkey <15859442+basharkey@users.noreply.github.com>
2022-07-17 13:12:20 -0400

    startup app launcher script

---
+++ .config/sway/scripts/startup-apps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+launch_app () {
+  echo -n "launching \"$1\""
+  swaymsg "exec $1"
+  
+  swaymsg -t get_tree | grep -q "$2"
+  while [ $? -ne 0 ]
+  do
+    sleep 0.1
+    swaymsg -t get_tree | grep -q "$2"
+  done
+}
+
+swaymsg "workspace 8"
+time launch_app keepassxc '"app_id": "org.keepassxc.KeePassXC"'
+
+swaymsg "workspace 7"
+time launch_app thunderbird '"class": "Thunderbird"'
+
+swaymsg "workspace 6"
+time launch_app "flatpak run io.freetubeapp.FreeTube" '"class": "FreeTube"'
+
+swaymsg "workspace 5"
+time launch_app "flatpak run com.discordapp.Discord" '"name": "Discord"'
+
+swaymsg "workspace 3"
