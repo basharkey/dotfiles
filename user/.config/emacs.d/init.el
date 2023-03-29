d81d9e6f95bc5db8ce3cfa1f83c9eda7647cd4c8
basharkey <15859442+basharkey@users.noreply.github.com>
2023-03-28 22:11:23 -0400

    magit and forge

--- user/.config/emacs.d/init.el
+++ user/.config/emacs.d/init.el
@@ -1,5 +1,6 @@
-(package-initialize)
 (require 'package)
+(package-initialize)
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
@@ -8,15 +9,35 @@
     (package-install package)))
 
 (use-package yaml-mode
-  :ensure t)
-(require 'yaml-mode)
-(setq auto-mode-alist
-      (append auto-mode-alist
-              '(("\\.yml\\'" . yaml-mode)
-                ("\\.yaml\\'" . yaml-mode))))
+  :ensure t
+  :mode ("\\.yml\\'"
+	 "\\.yaml\\'"))
 
+(use-package magit
+  :ensure t)
+
+(use-package forge
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+	
 (global-set-key (kbd "C-}") 'forward-paragraph)
 (global-set-key (kbd "C-{") 'backward-paragraph)
-
 (global-set-key (kbd "C-<") 'beginning-of-buffer)
 (global-set-key (kbd "C->") 'end-of-buffer)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ivy-mode t)
+ '(package-selected-packages '(yaml-mode use-package counsel))
+ '(scroll-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
