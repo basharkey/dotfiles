6a38e2a67844c6ab1d773bd9531b3c55e2ea8d67
basharkey <15859442+basharkey@users.noreply.github.com>
2024-06-28 15:30:52 -0400

    multi system override support

--- install.sh
+++ install.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
-cp -rT ./user/ ~/
-sudo cp -rT ./system/ /
+environment=$1
+override=$2
+
+if [ -z "$environment" ]; then
+    echo "Install dotfiles"
+    echo
+    echo "Usage: $0 user|system <override>"
+    echo "e.g. $0 user music"
+    echo
+    exit 1
+fi
+
+if [ "$environment" = "user" ]; then
+   cp -rT ./user/base ~/
+
+   if [ -n "$override" ]; then
+       if [ -d "./user/overrides/$override" ]; then
+	   cp -rT "./user/overrides/$override" ~/
+       else
+	   echo "Failed to copy \"user\" overrides, directory \"./user/overrides/$override\" not found"
+       fi
+   fi
+
+elif [ "$environment" = "system" ]; then
+    sudo cp -rT ./system/base /
+
+    if [ -n "$override"]; then
+	if [ -d "./system/overrides/$override" ]; then
+	    sudo cp -rT "./system/overrides/$override" /
+	else
+	    echo "Failed to copy \"system\" overrides, directory \"./system/overrides/$override\" not found"
+	fi
+    fi
+fi
