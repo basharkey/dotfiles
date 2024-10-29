868ea7366b076dfefd52a6848c5526462fc25572
basharkey <15859442+basharkey@users.noreply.github.com>
2024-10-29 14:31:11 -0400

    getopts install script

--- install.sh
+++ install.sh
@@ -1,35 +1,51 @@
 #!/usr/bin/env bash
-environment=$1
-override=$2
+
+usage() {
+    echo "Usage: $0 -e [user|system] [-o <override>]"
+    exit
+}
+
+while getopts "he:o:" opt; do
+    case "$opt" in
+	h)
+	    usage
+	    ;;
+	e)
+	    environment="$OPTARG"
+	    if [ "$environment" != "user" ] && [ "$environment" != "system" ]; then
+	       usage
+	    fi
+	    ;;
+	o)
+	    override="$OPTARG"
+	    ;;
+	*)
+	    usage
+	    ;;
+    esac
+done
 
 if [ -z "$environment" ]; then
-    echo "Install dotfiles"
-    echo
-    echo "Usage: $0 user|system <override>"
-    echo "e.g. $0 user music"
-    echo
-    exit 1
+    usage
 fi
+
+if [ -n "$override" ] && [ ! -d "./$environment/overrides/$override" ]; then
+    echo "overrides directory \"./$environment/overrides/$override\" not found"
+    usage
+fi
+
 
 if [ "$environment" = "user" ]; then
    cp -rT ./user/base ~/
 
    if [ -n "$override" ]; then
-       if [ -d "./user/overrides/$override" ]; then
-	   cp -rT "./user/overrides/$override" ~/
-       else
-	   echo "Failed to copy \"user\" overrides, directory \"./user/overrides/$override\" not found"
-       fi
+       cp -rT "./user/overrides/$override" ~/
    fi
 
 elif [ "$environment" = "system" ]; then
     sudo cp -rT ./system/base /
 
     if [ -n "$override" ]; then
-	if [ -d "./system/overrides/$override" ]; then
-	    sudo cp -rT "./system/overrides/$override" /
-	else
-	    echo "Failed to copy \"system\" overrides, directory \"./system/overrides/$override\" not found"
-	fi
+	sudo cp -rT "./system/overrides/$override" /
     fi
 fi
